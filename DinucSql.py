644a279b88a33e83bd413315b4851ad0c601cdd0
alvydas spokauskas <alspok@gmail.com>
2022-01-30 21:34:46 +0200

    .

--- DinucSql.py
+++ DinucSql.py
@@ -3,7 +3,7 @@ from ntpath import join
 from Classes.InitVars import InitVars as iv
 from Classes.GetColumn import GetColumn as gc
 from Classes.StringToFloat import StringToFloat
-import sqlite3
+from MatPlot import matPlot
 import sqlalchemy as db
 from pathlib import Path
 
@@ -12,8 +12,12 @@ def dinucSql() -> None:
     # di_diff_float = [float(di) for di in di_diff]
     di_diff_float = StringToFloat().stringToFloat(di_diff)
     di_shuffle_diff_float = StringToFloat().stringToFloat(di_shuffle_diff)
+    
+    matPlot(di_diff_float)
         
     pass
+
+
 
 
 if __name__ == "__main__":
---
+++ MatPlot.py
@@ -0,0 +1,6 @@
+def matPlot(float_list: list) -> None:
+    plot_list = []
+    for item in float_list:
+        for itm in item:
+            plot_list.append(itm)
+    pass
